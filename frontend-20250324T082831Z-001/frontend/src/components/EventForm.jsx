8e25c0d397e315bca592f8c76d551a6e7d552831
Abhi <abhivignesh27@gmail.com>
2025-03-24 14:44:07 +0530

    done

---
+++ frontend-20250324T082831Z-001/frontend/src/components/EventForm.jsx
@@ -0,0 +1,20 @@
+// src/components/EventForm.jsx
+import React from "react";
+
+
+
+function EventForm() {
+
+    return (
+        <>
+           <div>
+           <h1>name:{name}</h1>
+           <h2>date:{date}</h2>
+           <h2>loction:{loction}</h2>
+
+           </div>
+        </>
+    );
+}
+
+export default EventForm;
---
+++ frontend-20250324T082831Z-001/frontend/src/components/EventItem.jsx
@@ -0,0 +1,16 @@
+//EventItem Component
+import React from "react";
+
+
+function EventItem() {
+  return (
+    <div>
+      <h2>Event1</h2>
+      <h2>Event2</h2>
+      <h2>Event3</h2>
+      <h2>Event4</h2>
+    </div>
+  )
+}
+
+export default EventItem
